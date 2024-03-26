1cd39602d096f12928b15fcaa85b5d515d9f732c
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-03-26 03:12:14 +0100

    ajout favicon

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -11,6 +11,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             <div>
   </div>
           <Head>
+          <link rel="icon" href="/images/favicon.ico" sizes="any" />
           <link rel="preconnect" href="https://fonts.googleapis.com"  />
           <link rel="preconnect" href="https://fonts.gstatic.com"  />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
